736d548374ac198cb56fbc1ae408089aa6d304eb
angelfullstack <angelfullstack@gmail.com>
2020-03-08 14:51:48 +0100

    greada petición getone y su modelo getOne(id) para obtener un post por ID

--- models/post.js
+++ models/post.js
@@ -16,11 +16,21 @@ const getSome = (number)=>{
   });
 };
 
+const getOne = (id)=>{
+  return new Promise((resolve,reject)=>{
+    db.query("select * from posts where posts.id=?",[id],(err,rows)=>{
+      if(err) return reject(err);
+      if(rows[0]===undefined) return resolve('El post no existe');
+      return resolve(rows[0]);
+    })
+  })
+}
+
 const create = ({titulo,autor,categoria,contenido,imagen})=>{
   return new Promise ((resolve,reject)=>{
     db.query('INSERT INTO posts (titulo,autor,categoria,contenido,imagen,fecha) VALUES (?,?,?,?,?,?)',[titulo,autor,categoria,contenido,imagen,new Date()],
     (err,result)=>{
-      if(err) reject(err);
+      if(err) return reject(err);
       resolve(result);
     });
   });
@@ -29,5 +39,6 @@ const create = ({titulo,autor,categoria,contenido,imagen})=>{
 module.exports = {
   getAll: getAll,
   getSome:getSome,
+  getOne:getOne,
   create:create,
 };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,27 +2,35 @@ var router = require("express").Router();
 const Post = require("../../models/post");
 
 /*GET all Posts http://localhost:3000/api/posts/ */
-router.get("/", async (req, res) =>{
+router.get("/", async (req, res) => {
   const rows = await Post.getAll();
   res.json(rows);
 });
 
-
-router.get('/getsome', async (req,res)=>{
-  console.log(req.headers.number)
-  const rows= await Post.getSome(parseInt(req.headers.number));
+router.get("/getsome", async (req, res) => {
+  console.log(req.headers.number);
+  const rows = await Post.getSome(parseInt(req.headers.number));
   res.json(rows);
-})
-module.exports = router;
+});
 
-router.post('/', async(req,res)=>{
-  try{
+router.get("/getone", async (req, res) => {
+  console.log("Mira lo que tengo en el body" + req.body.id);
+  try {
+    const rows = await Post.getOne(parseInt(req.body.id));
+    res.json(rows);
+  } catch (err) {
+    console.log("el error es: " + err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
     console.log(req.body);
     const result = await Post.create(req.body);
     res.json(result);
+  } catch (error) {
+    console.log("El error es: " + error);
   }
-  catch(error){
-    console.log('El error es: '+error)
+});
 
-  }
-})
+module.exports = router;
